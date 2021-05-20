ff82f473ec3d59446179f53df2efc8ef2e3a00a5
mtende <justicemwanzamj@gmai.com>
2021-05-20 11:49:41 +0200

    Made changes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,23 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-router.get('/', ctrlMain.index);
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* other pages */
+router.get('/about', ctrlOthers.about);
+
+
 module.exports = router;
 
-/* GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-//module.exports = router;
-
-var homaPageController = function(req, res){
-     res.render('index', { title: 'Express'});
-};
-
-router.get('/', homaPageController);
-module.exports = router;*/
 
 
