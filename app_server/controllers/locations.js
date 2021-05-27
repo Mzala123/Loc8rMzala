940cec588c7d5143d4d00d6ce59d16f6a0c40134
mtende <justicemwanzamj@gmai.com>
2021-05-27 13:30:48 +0200

    Move view data to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,36 @@
 /* GET 'home' page */
 module.exports.homelist = function(req, res){
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', { 
+      title: 'Loc8r, Find a place to work with wifi',
+      pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'    
+    },
+    locations: [
+      {
+        name:'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: '3',
+        facilities: ['Hot Drinks', 'Food', 'Premium Wi-Fi'],
+        distance: '100m'
+      },
+      {
+        name:'Cafe Hero',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: '4',
+        facilities: ['Hot Drinks', 'Food', 'Premium Wi-Fi'],
+        distance: '200m'
+
+      },
+      {
+        name:'Bugger Queen',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: '2',
+        facilities: ['Hot Drinks', 'Food', 'Premium Wi-Fi'],
+        distance: '250m'
+      }
+    ]
+  });
   };
   
   /* GET 'Location info' page */
